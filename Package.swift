4d8fddecc0bb98e6a46ca19d0e50ca8f76193f97
Eugene Antropov <e@antropov.it>
2020-06-23 22:45:40 +0300

    Make static

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .iOS(.v9),
     ],
     products: [
-        .library(name: "SBTUITestTunnelClient", type: .dynamic, targets: ["SBTUITestTunnelClient"]),
-        .library(name: "SBTUITestTunnelServer", type: .dynamic, targets: ["SBTUITestTunnelServer"]),
+        .library(name: "SBTUITestTunnelClient", type: .static, targets: ["SBTUITestTunnelClient"]),
+        .library(name: "SBTUITestTunnelServer", type: .static, targets: ["SBTUITestTunnelServer"]),
     ],
     dependencies: [
         .package(name: "GCDWebServer", url: "https://github.com/SlaunchaMan/GCDWebServer.git",.branch("swift-package-manager")),
